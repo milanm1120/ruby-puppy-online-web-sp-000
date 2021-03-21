32af2c31d76eed9963f21ed00b3092f2a1566791
Milan Mody <milanm1120@gmail.com>
2021-03-21 01:50:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,7 +8,7 @@ class Dog
 
   def initialize(name)
     @name = name
-    @@all << self
+    save
   end
 
   def self.all
@@ -23,6 +23,10 @@ class Dog
     @@all.each do |dog|
       puts dog.name
     end
+  end
+
+  def save
+    @@all << self
   end
 
 end
