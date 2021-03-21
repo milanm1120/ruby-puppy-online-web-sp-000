d30fdcb223b3730cc8f4bd1705a2ec1e6553004d
Milan Mody <milanm1120@gmail.com>
2021-03-21 01:40:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,30 +1,67 @@
 # Add your code here
-class Dog
 
-  @@all = []
 
-  attr_accessor :name
-  def initialize(name)
-    @name = name
-    save
-  end
 
-  def save
-    @@all << self
-  end
 
-  def self.all
-    @@all
-  end
 
-  def self.clear_all
-    @@all.clear
-  end
 
-  def self.print_all
-    @@all.each do |dog|
-      puts dog.name
-    end
-  end
 
-end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Dog
+#
+#   @@all = []
+#
+#   attr_accessor :name
+#   def initialize(name)
+#     @name = name
+#     save
+#   end
+#
+#   def save
+#     @@all << self
+#   end
+#
+#   def self.all
+#     @@all
+#   end
+#
+#   def self.clear_all
+#     @@all.clear
+#   end
+#
+#   def self.print_all
+#     @@all.each do |dog|
+#       puts dog.name
+#     end
+#   end
+#
+# end
